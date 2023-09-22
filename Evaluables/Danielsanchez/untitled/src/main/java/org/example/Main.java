87b04047a7b967f7bc86507c3c488d6472a09fd4
Danielsanchez <dansanher@alu.edu.gva.es>
2023-09-22 18:42:39 +0200

    esto es de prueba

--- Evaluables/Danielsanchez/untitled/src/main/java/org/example/Main.java
+++ Evaluables/Danielsanchez/untitled/src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-
+    // DASD
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
