9bf9e55817626f037d77f76dbea46e9de9268137
aaroncr6 <aaroncutillas@gmail.com>
2023-09-27 17:08:59 +0200

    añado archivos mp3 y  comienzo los files para las rutas

--- Evaluables/Aaron/Ejercicio02/src/main/java/org/example/Main.java
+++ Evaluables/Aaron/Ejercicio02/src/main/java/org/example/Main.java
@@ -1,20 +1,27 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws FileNotFoundException {
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        try {
+            File rutaFileMusica = new File("src/main/resources/rutaFileMusica.txt");
+            if(!rutaFileMusica.exists()){
+                rutaFileMusica.createNewFile();
+            }
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);//hola
+            File rutaMP3 = new File("src/main/resources/musica");
+            File [] listaRuta = rutaMP3.listFiles();
 
+
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
         }
+
+
     }
 }
