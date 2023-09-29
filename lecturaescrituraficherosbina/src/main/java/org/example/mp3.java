66c0bd90c861731ef708e8b53fd7ba5cca800405
joseluisbotelladevesa <botelladevesajoseluis@gmail.com>
2023-09-29 19:20:22 +0200

    siiiiiii

--- lecturaescrituraficherosbina/src/main/java/org/example/mp3.java
+++ lecturaescrituraficherosbina/src/main/java/org/example/mp3.java
@@ -1,24 +1,118 @@
 package org.example;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class mp3 {
 
 
-    public static void LeerInfoMP3(String nombreArchivo){
-        try (FileReader fileReader = new FileReader(nombreArchivo);
+    public static void LeerInfoMP3(String nombreArchivo) {
+        File archivo=new File("src/main/resources/"+nombreArchivo+".txt");
 
-             BufferedReader buffer= new BufferedReader(fileReader)) {
+        try (FileReader fileReader = new FileReader(archivo);
+
+             BufferedReader buffer = new BufferedReader(fileReader)) {
 
             ArrayList<String> lineas = new ArrayList<>();
             String linea;
             while ((linea = buffer.readLine()) != null) {
                 lineas.add(linea);
             }
+    buffer.close();
 
-        }catch (Exception ex){
-            System.out.println("Error"+ex.getMessage());
+for (String line : lineas) {
+File fil=new File(line);
+
+    try (RandomAccessFile raf = new RandomAccessFile(fil, "r")) {
+        long longitudArchivo = raf.length();
+        long posicionMetadatos =  longitudArchivo - 128;
+        raf.seek(posicionMetadatos);
+
+// Leer los metadatos
+
+        byte[] cabecera = new byte[3];
+        raf.read(cabecera);
+        String cabec = new String(cabecera);
+
+        byte[] titulo = new byte[30];
+        raf.read(titulo);
+        String titu = new String(titulo);
+
+        byte[] artista = new byte[30];
+        raf.read(artista);
+        String arti = new String(artista);
+
+        byte[] album = new byte[30];
+        raf.read(album);
+        String alb = new String(album);
+
+        byte[] anyo = new byte[4];
+        raf.read(anyo);
+        String any = new String(anyo);
+
+        byte[] comentario = new byte[28];
+        raf.read(comentario);
+        String come = new String(comentario);
+
+        byte[] tienenp = new byte[1];
+        raf.read(tienenp);
+        String tnp = new String(tienenp);
+
+        byte[] numpista = new byte[1];
+        raf.read(numpista);
+        String nump = new String(numpista);
+
+        byte[] genero = new byte[4];
+        raf.read(genero);
+        String gene = new String(genero);
+
+
+           /*     String titulo = new String(bufer, 3, 30, "UTF-8").trim();
+                String artista = new String(bufer, 33, 30, "UTF-8").trim();
+                String album = new String(bufer, 63, 30, "UTF-8").trim();
+                String año = new String(bufer, 93, 4, "UTF-8").trim();
+                String comentario = new String(bufer, 97, 28, "UTF-8").trim(); */
+        System.out.println(titu.toString());
+        System.out.println(cabec.toString());
+        System.out.println(arti.toString());
+        System.out.println(alb.toString());
+        System.out.println(anyo.toString());
+        System.out.println(come.toString());
+        System.out.println(nump.toString());
+        System.out.println(gene.toString());
+
+    } catch (Exception ex) {
+        System.out.println("Error metadatos");
+    }
+}
+
+
+
+
+
+            }catch(Exception ex){
+                System.out.println("Error" + ex.getMessage());
+            }
+
+        }
+
+
+    public static void contarBytesYAlmacenar(String nombreArchivoMP3) throws IOException {
+
+
+        File archivo = new File(nombreArchivoMP3);
+
+
+        if (archivo.exists()) {
+            long tamanoBytes = archivo.length();
+            System.out.println("Archivo MP3: " + nombreArchivoMP3);
+            System.out.println("Tamaño en bytes: " + tamanoBytes);
+
+            // Aquí puedes almacenar el tamaño en bytes o realizar otras operaciones según tus necesidades.
+        } else {
+            System.out.println("El archivo MP3 no existe: " + nombreArchivoMP3);
         }
 
     }
@@ -28,7 +122,8 @@ public class mp3 {
         try {
             File rutaArchivo=new File("src/main/resources/"+nombreArchivo);
             System.out.println("Primera Cancion");
-            DataOutputStream salida= new DataOutputStream(rutaArchivo);
+            FileOutputStream fileout=new FileOutputStream(rutaArchivo);
+            DataOutputStream salida= new DataOutputStream(fileout);
 
 
 
