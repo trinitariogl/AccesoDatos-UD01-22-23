41aed788e4dbf6cb7609f39e93197ad960b49b7c
Daniel Garcia Pascual <danielgarciapascual23@gmail.com>
2023-10-03 13:05:10 +0200

    cambios pesistencia

--- Evaluables/Daniel Garcia/Persistencia Objetos/src/main/java/org/example/Main.java
+++ Evaluables/Daniel Garcia/Persistencia Objetos/src/main/java/org/example/Main.java
@@ -11,43 +11,92 @@ import java.util.Scanner;
 public class Main {
 
     static Scanner sc = new Scanner(System.in);
-
+    static List<Pelicula> peliculas = new ArrayList<>();
     public static void main(String[] args) {
 
-         Pelicula p = leerDatosTeclado();
-         insertarObjeto(p);
+        Menu menu = new Menu();
 
-        List<Pelicula> pelis =  leerPeliculasFichero();
-        for (Pelicula peli: pelis){
+        int opcion;
+        System.out.println("*** Mi VideoClub ***");
+
+        do {
+            menu.mostrar();
+
+            opcion = menu.leerOpcion();
+
+            switch (opcion){
+                case 1:
+                    leerDatosTeclado();
+                    break;
+                case 2:
+                    modificarPelicula();
+                    break;
+                case 3:
+                    eliminarPelicula();
+                    break;
+                case 4:
+                    visualizarPelicula();
+                    break;
+                case 5:
+                    visualizarTodasLasPeliculas();
+                    break;
+                case 6:
+                    System.out.println("FIN DEL PROGRAMA");
+                    break;
+            }
+
+        } while (opcion != 6);
+
+        /*
+        Pelicula p = leerDatosTeclado();
+        //insertarObjeto(p);
+
+        peliculas =  leerPeliculasFichero();
+        for (Pelicula peli: peliculas){
             System.out.println(peli);
-        }
+        }*/
 
     }
 
-    public static Pelicula leerDatosTeclado(){
+    public static void leerDatosTeclado(){
 
         DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         Pelicula peli = new Pelicula();
+        int num;
+        peliculas = leerPeliculasFichero();
 
-        System.out.print("Introduzca el título: ");
-        peli.setTitulo(sc.nextLine());
+        do {
+            System.out.print("Cuantas peliculas deseas almacenar? ");
+            num = sc.nextInt();
+        }while (num < 0);
 
-        System.out.print("Introduzca actores: ");
-        peli.setActores(Arrays.asList(sc.nextLine().split(";")));
+        sc.nextLine();
 
-        System.out.print("Introduzca directores: ");
-        peli.setDirectores(Arrays.asList(sc.nextLine().split(";")));
+        for (int i = 1; i <= num; i++) {
+            System.out.println("Peliucula " + i + ":");
+            System.out.print("Introduzca el título: ");
+            peli.setTitulo(sc.nextLine());
 
-        System.out.print("Introduzca fecha de salida: ");
-        peli.setFechaSalida(LocalDate.parse(sc.nextLine(), formatoFecha));
+            System.out.print("Introduzca actores: ");
+            peli.setActores(Arrays.asList(sc.nextLine().split(";")));
 
-        System.out.print("Introduce formato: ");
-        peli.setFormato(sc.nextLine());
+            System.out.print("Introduzca directores: ");
+            peli.setDirectores(Arrays.asList(sc.nextLine().split(";")));
 
-        return peli;
+            System.out.print("Introduzca fecha de salida: ");
+            peli.setFechaSalida(LocalDate.parse(sc.nextLine(), formatoFecha));
+
+            System.out.print("Introduce formato: ");
+            peli.setFormato(sc.nextLine());
+            peliculas.add(peli);
+        }
+
+
+        insertarObjetos();
+
     }
 
-    public static void insertarObjeto(Pelicula newFilm) {
+    public static void insertarObjetos() {
 
         try{
             File archivo = new File("src/main/resources/peliculas.dat");
@@ -57,7 +106,12 @@ public class Main {
             FileOutputStream escritura = new FileOutputStream(archivo);
             ObjectOutputStream objectOut = new ObjectOutputStream(escritura);
 
-            objectOut.writeObject(newFilm);
+
+
+            for (Pelicula newFilm: peliculas){
+                objectOut.writeObject(newFilm);
+            }
+
             objectOut.close();
         } catch (IOException ex){
             System.out.println("Error en fichero -> " + ex.getMessage());
@@ -69,7 +123,7 @@ public class Main {
 
     public static List<Pelicula> leerPeliculasFichero() {
 
-        List<Pelicula> listaPeliculas = null;
+        List<Pelicula> listaPeliculas;
         try {
             listaPeliculas = new ArrayList<>();
             File archivo = new File("src/main/resources/peliculas.dat");
@@ -84,7 +138,6 @@ public class Main {
 
             }  catch (EOFException eof){
                 objectInput.close();
-                System.out.println("Se ha leído correctamente.");
             }
 
         }catch (IOException ex){
@@ -96,5 +149,85 @@ public class Main {
         }
 
         return listaPeliculas;
+    }
+
+    public static void modificarPelicula(){
+        String tit, formato;
+        boolean existe = false;
+
+        System.out.print("Introduce el titulo de la pelicula que quieras modificar el formato: ");
+        tit = sc.nextLine();
+
+        peliculas = leerPeliculasFichero();
+
+        for (Pelicula p: peliculas){
+            if(p.getTitulo().equalsIgnoreCase(tit)){
+                System.out.print("Introduce el nuevo formato de la pelicula: ");
+                formato = sc.nextLine();
+                p.setFormato(formato);
+                existe = true;
+            }
+        }
+
+        if (!existe){
+            System.out.println("La pelicula con titulo \"" + tit + "\" no existe.");
+        }
+
+        insertarObjetos();
+    }
+
+    public static void eliminarPelicula(){
+        String tit;
+        boolean existe = false;
+
+        System.out.print("Introduce el titulo de la pelicula que quieras eliminar: ");
+        tit = sc.nextLine();
+
+        peliculas = leerPeliculasFichero();
+
+        for (Pelicula p: peliculas){
+            if(p.getTitulo().equalsIgnoreCase(tit)){
+                peliculas.remove(p);
+                existe = true;
+                System.out.println("La pelicula \"" + tit + "\" se ha eliminado correctamente");
+            }
+        }
+
+        if (!existe){
+            System.out.println("La pelicula con titulo \"" + tit + "\" no existe.");
+        }
+
+        insertarObjetos();
+    }
+
+    public static void visualizarPelicula(){
+        String tit;
+        boolean existe = false;
+
+        System.out.print("Introduce el titulo de la pelicula que quieras visualizar: ");
+        tit = sc.nextLine();
+
+        peliculas = leerPeliculasFichero();
+
+        for (Pelicula p: peliculas) {
+            if (p.getTitulo().equalsIgnoreCase(tit)) {
+                System.out.println(p);
+                existe = true;
+            }
+        }
+
+        if (!existe){
+            System.out.println("La pelicula con titulo \"" + tit + "\" no existe.");
+        }
+
+
+    }
+
+    public static void visualizarTodasLasPeliculas(){
+        System.out.println("Listado con todas las peliculas de nuestra base de datos.");
+        peliculas =  leerPeliculasFichero();
+        for (Pelicula peli: peliculas){
+            System.out.println(peli);
+        }
     }
 }
