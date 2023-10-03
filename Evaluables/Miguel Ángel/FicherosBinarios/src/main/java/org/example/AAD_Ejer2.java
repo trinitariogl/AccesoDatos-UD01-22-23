23499f6ad848febe1c7a3ed3e6772b52d1344119
Miguel Ángel <miguelangelmartinezguijarro@gmail.com>
2023-10-03 17:23:34 +0200

    Persistencia de Objetos completa

--- Evaluables/Miguel Ángel/FicherosBinarios/src/main/java/org/example/AAD_Ejer2.java
+++ Evaluables/Miguel Ángel/FicherosBinarios/src/main/java/org/example/AAD_Ejer2.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
+import java.nio.charset.StandardCharsets;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -11,37 +12,6 @@ public class AAD_Ejer2 {
     static int opcion = 0;
     static Scanner sc = new Scanner (System.in);
     public static void main(String[] args) throws FileNotFoundException {
-
-        File fichero = new File("src/main/resources/musica.mp3");
-
-        try (FileInputStream lectura = new FileInputStream(fichero)) {
-
-            RandomAccessFile aleatorio =
-                    new RandomAccessFile(fichero,"rw");
-            aleatorio.seek(aleatorio.length() - 128);
-
-            StringBuffer sb = new StringBuffer(3);
-
-            String nombre = "";
-
-            for (int i = 0; i < 3; i++) {
-                aleatorio.seek(aleatorio.getFilePointer() + 1);
-                sb.append(aleatorio.getFilePointer());
-            }
-
-            System.out.println(sb);
-
-            for (; aleatorio.getFilePointer() <= aleatorio.length(); aleatorio.seek(aleatorio.getFilePointer() + 1)) {
-                sb.append(aleatorio.getFilePointer());
-            }
-            System.out.println(sb);
-
-        } catch (FileNotFoundException e2) {
-            System.out.println("No se encontro na chicquillos");
-        }  catch (Exception e) {
-            System.out.println("Error" + e.getMessage());
-        }
-
         do{
             try{
                 System.out.println("\n** GESTIÓN MÚSICA MP3 **");
@@ -69,14 +39,28 @@ public class AAD_Ejer2 {
                             nombreFichero = sc.nextLine();
 
                                 if (nombreFichero.isEmpty() || nombreFichero.isBlank()) {
-                                    System.err.println("El nombre del fichero no puede etar en blanco");
+                                    System.err.println("El nombre del fichero no puede estar en blanco");
                                 }
 
                         } while (nombreFichero.isBlank() || nombreFichero.isEmpty());
 
+                        MP3.LeerInfoMP3(nombreFichero);
+
                         break;
                     case 2:
-                        System.out.println("caso 2");
+                        
+                        String ficheroNombre = "";
+                        do{
+                            System.out.println("\nNombre del fichero que deseas mostrar su información: ");
+                            System.out.print("Nombre: ");
+                            ficheroNombre = sc.nextLine();
+
+                            if (ficheroNombre.isEmpty() || ficheroNombre.isBlank()) {
+                                System.err.println("El nombre del fichero no puede estar en blanco");
+                            }
+
+                        } while (ficheroNombre.isBlank() || ficheroNombre.isEmpty());
+
                         break;
                     case 3:
                         System.out.println("Fin del programa");
